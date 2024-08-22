ec0841b9da36c0e38604352c0fb064ab208bb7a3
hp <email@example.com>
2024-08-23 01:10:00 +0300

    slider works

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -7,12 +7,19 @@ public class UIManager : MonoBehaviour
     public Button rightButton;
     public VehicleSpawner vehicleSpawner;
     private int laneIndex = 0; // Varsayılan lane index (0: Left, 1: Right)
+    public Image[] vehicleImages; // Araçları temsil eden resimler
 
     void Start()
     {
         // Butonlara tıklama olaylarını bağlayın
         leftButton.onClick.AddListener(SetLaneIndexLeft);
         rightButton.onClick.AddListener(SetLaneIndexRight);
+        // Her bir resim için tıklanma olayını bağla
+        for (int i = 0; i < vehicleImages.Length; i++)
+        {
+            int index = i; // İç içe lambda kullanımı için yerel bir kopya oluştur
+            vehicleImages[i].GetComponent<Button>().onClick.AddListener(() => OnVehicleImageClicked(index));
+        }
     }
 
     void SetLaneIndexLeft()
@@ -27,5 +34,10 @@ public class UIManager : MonoBehaviour
         laneIndex = 1; // Right lane
         vehicleSpawner.laneIndex = laneIndex;
         Debug.Log("Lane Index set to: " + laneIndex);
+    }
+    private void OnVehicleImageClicked(int index)
+    {
+        // Tıklanan resmin indexine göre aracı spawn et
+        vehicleSpawner.SpawnVehicle(index);
     }
 }
--- Assets/Scripts/Vehicle/VehicleSpawner.cs
+++ Assets/Scripts/Vehicle/VehicleSpawner.cs
@@ -35,20 +35,15 @@ public class VehicleSpawner : MonoBehaviour
             return;
         }
 
-        // Doğru PathCreator'ı seç
-        PathCreator selectedPathCreator = pathCreators[laneIndex]; // laneIndex değerine göre doğru path seçiliyor
-
-        // Yeni bir araç oluştur
-        Vehicle newVehicle = Instantiate(vehiclePrefab, 
-            selectedPathCreator.path.GetPointAtDistance(startDistance), 
-            selectedPathCreator.path.GetRotationAtDistance(startDistance));
-
-        // Araç veri ayarlarını yap
         VehicleData data = vehicleDatas[index];
-        newVehicle.vehicleData = data;
 
-        // Araç üzerinde gerekli ayarlamaları yap
+        // Doğru PathCreator'ı seç
+        PathCreator selectedPathCreator = pathCreators[laneIndex];
+
+        // Yeni bir araç oluştur ve doğru pozisyonda başlat
+        Vehicle newVehicle = Instantiate(vehiclePrefab, selectedPathCreator.path.GetPointAtDistance(startDistance), selectedPathCreator.path.GetRotationAtDistance(startDistance));
+        newVehicle.vehicleData = data;
         newVehicle.pathCreator = selectedPathCreator;
-        newVehicle.distanceTravelled = startDistance;  // Araç başlangıç noktasında
+        newVehicle.distanceTravelled = startDistance;
     }
 }
